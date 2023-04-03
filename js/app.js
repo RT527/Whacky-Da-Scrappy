15fa028be522a8e90c2754edb24e7108bcb9bfba
RT527 <rafi.talukder7@gmail.com>
2023-04-03 16:01:38 -0400

    add more updates for js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const dogs = { 
+  Scooby: {
+    image: 'Scrappy_files/Scooby-happy.png',
+    whackedImage: 'Scrappy_files/Scooby-hurt.png',
+    score: -10,
+    soundGroup: 'Scooby'
+  },
+  Scrappy : {
+    image: 'Scrappy_files/Scrappy-happy.png',
+    whackedImage: 'Scrappy_files/Scrappy-hurt.png',
+    score: 10,
+    soundGroup: 'Scrappy'
+  }
+}
+const sounds = {
+  Scooby: [
+    'Scrappy_files/Scooby-1.mp3',
+    'Scrappy_files/Scooby-2.mp3'
+  ],
+  Scrappy: [
+    'Scrappy_files/Scrappy-1.mp3',
+    'Scrappy_files/Scrappy-2.mp3',
+    'Scrappy_files/Scrappy-3.mp3'
+  ]
+}
 
 /*---------------------------- Variables (state) ----------------------------*/
 let score = 0
@@ -8,13 +32,8 @@ let intervalID = null
 
 /*------------------------ Cached Element References ------------------------*/
 const cursor = document.querySelector(".cursor")
-const sounds = new Audio('Scrappy_files/Scooby-1.mp3') 
-const sound = new Audio(sounds[dog.])
-const dogs = [
-  {
-    image: 'Scrappy_files/'
-  }
-]
+const holes = [...document.querySelectorAll('hole')]
+const scoreEl = document.querySelector('.score span')
 
 /*----------------------------- Event Listeners -----------------------------*/
 window.addEventListener('mousemove', m => {
@@ -29,6 +48,3 @@ window.addEventListener('mouseup',() =>{
 })
 /*-------------------------------- Functions --------------------------------*/
 
-window.addEventListener('click',()=>{
-  const sound
-})
