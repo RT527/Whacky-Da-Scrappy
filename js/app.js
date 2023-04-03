199e8eba7cf2e51daa8c0ea9cca46dfd45bd1e90
RT527 <rafi.talukder7@gmail.com>
2023-04-03 15:33:41 -0400

    added some functionality like noise

--- js/app.js
+++ js/app.js
@@ -2,12 +2,19 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let score = 0
+let timeLeft = 60
+let intervalID = null
 
 /*------------------------ Cached Element References ------------------------*/
 const cursor = document.querySelector(".cursor")
-
-
+const sounds = new Audio('Scrappy_files/Scooby-1.mp3') 
+const sound = new Audio(sounds[dog.])
+const dogs = [
+  {
+    image: 'Scrappy_files/'
+  }
+]
 
 /*----------------------------- Event Listeners -----------------------------*/
 window.addEventListener('mousemove', m => {
@@ -20,6 +27,8 @@ window.addEventListener('mousedown',() =>{
 window.addEventListener('mouseup',() =>{
   cursor.classList.remove('active')
 })
-
-resetBtnEl.addEventListener('click',init)
 /*-------------------------------- Functions --------------------------------*/
+
+window.addEventListener('click',()=>{
+  const sound
+})
