bc02a74aa16c9a20d05549dc816e1904079ab995
RT527 <rafi.talukder7@gmail.com>
2023-04-03 11:08:32 -0400

    cursor upgraded

--- js/app.js
+++ js/app.js
@@ -1,2 +1,25 @@
-console.log("SCOOBY DOOBY DOOO!")
-console.log("I hate scrappy doo ong")
+/*-------------------------------- Constants --------------------------------*/
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+/*------------------------ Cached Element References ------------------------*/
+const cursor = document.querySelector(".cursor")
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+window.addEventListener('mousemove', m => {
+  cursor.style.top = m.pageY + 'px'
+  cursor.style.left = m.pageX + 'px'
+})
+window.addEventListener('mousedown',() =>{
+  cursor.classList.add('active')
+})
+window.addEventListener('mouseup',() =>{
+  cursor.classList.remove('active')
+})
+
+resetBtnEl.addEventListener('click',init)
+/*-------------------------------- Functions --------------------------------*/
